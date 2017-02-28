d6ced1c2117316c2f9fbbe271a9a58103b5c1e66
Kira <me@imkira.com>
2017-02-28 18:45:28 +0800

    add metadata apis of space and chart

--- pkg/api/v1/descriptor/manifests.go
+++ pkg/api/v1/descriptor/manifests.go
@@ -22,11 +22,133 @@ func init() {
 // manifests descriptors
 var manifests = []definition.Descriptor{
 	{
+		Path: "/spaces/{space}/metadata",
+		Handlers: []definition.Handler{
+			{
+				HTTPMethod: http.MethodGet,
+				Handler:    definition.NewHandlerDecoration(definition.VerbList, handlers.ListMetadataInSpace).Handle,
+				Doc:        "List all metadata in a space",
+				PathParams: []definition.Param{
+					{
+						Name:     "space",
+						Type:     "string",
+						Doc:      "space name",
+						Required: true,
+					},
+				},
+				QueryParams: []definition.Param{
+					{
+						Name:     "start",
+						Type:     "number",
+						Doc:      "Query start index",
+						Required: false,
+						Default:  0,
+					},
+					{
+						Name:     "limit",
+						Type:     "number",
+						Doc:      "Specify the number of records to return",
+						Required: false,
+						Default:  common.DefaultPagingLimit,
+					},
+				},
+				StatusCode: []definition.StatusCode{
+					definition.StatusCode{Code: http.StatusOK, Message: "Success and respond with a array of metadata",
+						Sample: &models.ListResponse{
+							Metadata: models.Metadata{
+								Total:       10,
+								ItemsLength: 1,
+							},
+							Items: []*storage.Metadata{
+								{
+									Metadata: chart.Metadata{
+										Name:        "A",
+										Version:     "1.0.0",
+										Description: "A chart named A and has dependency with ChartB",
+									},
+									Dependencies: []*storage.Metadata{
+										{
+											Metadata: chart.Metadata{
+												Name:        "B",
+												Version:     "2.1.0",
+												Description: "A chart is named B",
+											},
+										},
+									},
+								},
+							},
+						}},
+				},
+			},
+		},
+	},
+	{
+		Path: "/spaces/{space}/metadata/latest",
+		Handlers: []definition.Handler{
+			{
+				HTTPMethod: http.MethodGet,
+				Handler:    definition.NewHandlerDecoration(definition.VerbList, handlers.ListLatestMetadataInSpace).Handle,
+				Doc:        "List latest metadata in a space",
+				PathParams: []definition.Param{
+					{
+						Name:     "space",
+						Type:     "string",
+						Doc:      "space name",
+						Required: true,
+					},
+				},
+				QueryParams: []definition.Param{
+					{
+						Name:     "start",
+						Type:     "number",
+						Doc:      "Query start index",
+						Required: false,
+						Default:  0,
+					},
+					{
+						Name:     "limit",
+						Type:     "number",
+						Doc:      "Specify the number of records to return",
+						Required: false,
+						Default:  common.DefaultPagingLimit,
+					},
+				},
+				StatusCode: []definition.StatusCode{
+					definition.StatusCode{Code: http.StatusOK, Message: "Success and respond with a array of latest metadata",
+						Sample: &models.ListResponse{
+							Metadata: models.Metadata{
+								Total:       10,
+								ItemsLength: 1,
+							},
+							Items: []*storage.Metadata{
+								{
+									Metadata: chart.Metadata{
+										Name:        "A",
+										Version:     "1.0.0",
+										Description: "A chart named A and has dependency with ChartB",
+									},
+									Dependencies: []*storage.Metadata{
+										{
+											Metadata: chart.Metadata{
+												Name:        "B",
+												Version:     "2.1.0",
+												Description: "A chart is named B",
+											},
+										},
+									},
+								},
+							},
+						}},
+				},
+			},
+		},
+	},
+	{
 		Path: "/spaces/{space}/charts/{chart}/metadata",
 		Handlers: []definition.Handler{
 			{
 				HTTPMethod: http.MethodGet,
-				Handler:    definition.NewHandlerDecoration(definition.VerbList, handlers.ListMetadata).Handle,
+				Handler:    definition.NewHandlerDecoration(definition.VerbList, handlers.ListMetadataInChart).Handle,
 				Doc:        "List all metadata in a chart",
 				PathParams: []definition.Param{
 					{
@@ -80,6 +202,49 @@ var manifests = []definition.Descriptor{
 												Description: "A chart is named B",
 											},
 										},
+									},
+								},
+							},
+						}},
+				},
+			},
+		},
+	},
+	{
+		Path: "/spaces/{space}/charts/{chart}/metadata/latest",
+		Handlers: []definition.Handler{
+			{
+				HTTPMethod: http.MethodGet,
+				Handler:    definition.NewHandlerDecoration(definition.VerbGet, handlers.GetLatestMetadataInChart).Handle,
+				Doc:        "Get metadata of the latest version in a chart",
+				PathParams: []definition.Param{
+					{
+						Name:     "space",
+						Type:     "string",
+						Doc:      "space name",
+						Required: true,
+					},
+					{
+						Name:     "chart",
+						Type:     "string",
+						Doc:      "chart name",
+						Required: true,
+					},
+				},
+				StatusCode: []definition.StatusCode{
+					definition.StatusCode{Code: http.StatusOK, Message: "Success and respond with metadata of latest version",
+						Sample: &storage.Metadata{
+							Metadata: chart.Metadata{
+								Name:        "A",
+								Version:     "1.0.0",
+								Description: "A chart named A and has dependency with ChartB",
+							},
+							Dependencies: []*storage.Metadata{
+								{
+									Metadata: chart.Metadata{
+										Name:        "B",
+										Version:     "2.1.0",
+										Description: "A chart is named B",
 									},
 								},
 							},
--- pkg/api/v1/handlers/manifests.go
+++ pkg/api/v1/handlers/manifests.go
@@ -8,12 +8,72 @@ import (
 	"context"
 
 	"github.com/caicloud/helm-registry/pkg/common"
+	"github.com/caicloud/helm-registry/pkg/errors"
 	"github.com/caicloud/helm-registry/pkg/storage"
 )
 
-// ListMetadata lists all metadata of a chart
-func ListMetadata(ctx context.Context) (int, []*storage.Metadata, error) {
+// ListMetadataInSpace lists all metadata in a space
+func ListMetadataInSpace(ctx context.Context) (int, []*storage.Metadata, error) {
+	spaceName, err := getSpaceName(ctx)
+	if err != nil {
+		return 0, nil, err
+	}
+	start, limit, err := getPaging(ctx)
+	if err != nil {
+		return 0, nil, err
+	}
+	space, err := common.GetSpace(ctx, spaceName)
+	if err != nil {
+		return 0, nil, err
+	}
+	metadata, err := space.VersionMetadata(ctx)
+	if err != nil {
+		return 0, nil, err
+	}
+	total := len(metadata)
+	start, end := standardizeRange(total, start, limit)
+	return total, metadata[start:end], nil
+}
+
+// ListLatestMetadataInSpace lists all metadata of the latest version of charts in space
+func ListLatestMetadataInSpace(ctx context.Context) (int, []*storage.Metadata, error) {
+	spaceName, err := getSpaceName(ctx)
+	if err != nil {
+		return 0, nil, err
+	}
+	start, limit, err := getPaging(ctx)
+	if err != nil {
+		return 0, nil, err
+	}
+	space, err := common.GetSpace(ctx, spaceName)
+	if err != nil {
+		return 0, nil, err
+	}
+	chartNames, err := space.List(ctx)
+	if err != nil {
+		return 0, nil, err
+	}
+	metadata := make([]*storage.Metadata, 0, len(chartNames))
+	for _, chartName := range chartNames {
+		md, err := getLatestMetadata(ctx, spaceName, chartName)
+		if err != nil {
+			return 0, nil, err
+		}
+		metadata = append(metadata, md)
+	}
+
+	total := len(metadata)
+	start, end := standardizeRange(total, start, limit)
+	return total, metadata[start:end], nil
+}
+
+// ListMetadataInChart lists all metadata in a chart
+func ListMetadataInChart(ctx context.Context) (int, []*storage.Metadata, error) {
 	spaceName, chartName, err := getSpaceAndChartName(ctx)
+	if err != nil {
+		return 0, nil, err
+	}
+	start, limit, err := getPaging(ctx)
 	if err != nil {
 		return 0, nil, err
 	}
@@ -26,13 +86,18 @@ func ListMetadata(ctx context.Context) (int, []*storage.Metadata, error) {
 	if err != nil {
 		return 0, nil, err
 	}
-	start, limit, err := getPaging(ctx)
-	if err != nil {
-		return 0, nil, err
-	}
 	total := len(metadata)
 	start, end := standardizeRange(total, start, limit)
 	return total, metadata[start:end], nil
+}
+
+// GetLatestMetadataInChart gets metadata of the latest version in a chart
+func GetLatestMetadataInChart(ctx context.Context) (metadata *storage.Metadata, err error) {
+	spaceName, chartName, err := getSpaceAndChartName(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return getLatestMetadata(ctx, spaceName, chartName)
 }
 
 // FetchMetadata fetches metadata of specified version
@@ -51,4 +116,24 @@ func FetchValues(ctx context.Context) (data []byte, err error) {
 		return err
 	})
 	return
+}
+
+// getLatestMetadata gets latest metadata in a chart
+func getLatestMetadata(ctx context.Context, spaceName, chartName string) (metadata *storage.Metadata, err error) {
+	chart, err := common.GetChart(ctx, spaceName, chartName)
+	if err != nil {
+		return nil, err
+	}
+	versionNumbers, err := chart.List(ctx)
+	if err != nil {
+		return nil, err
+	}
+	if len(versionNumbers) <= 0 {
+		return nil, errors.ErrorContentNotFound.Format("metadata")
+	}
+	version, err := chart.Version(ctx, versionNumbers[len(versionNumbers)-1])
+	if err != nil {
+		return nil, err
+	}
+	return version.Metadata(ctx)
 }
